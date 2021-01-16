825fd64b8dbcac4a9f67393375f1994d02661f56
thec0sm0s <deepakrajko14@gmail.com>
2021-01-16 22:42:14 +0530

    Prettify stuffs

--- test/set-cookie-parser.js
+++ test/set-cookie-parser.js
@@ -88,10 +88,15 @@ describe("set-cookie-parser", function () {
   });
 
   it("should handle the case when value is not UTF-8 encoded", function () {
-    var cookieStr = "foo=R%F3r%EB%80%8DP%FF%3B%2C%23%9A%0CU%8E%A2C8%D7%3C%3C%B0%DF%17%60%F7Y%DB%16%8BQ%D6%1A";
+    var cookieStr =
+      "foo=R%F3r%EB%80%8DP%FF%3B%2C%23%9A%0CU%8E%A2C8%D7%3C%3C%B0%DF%17%60%F7Y%DB%16%8BQ%D6%1A";
     var actual = setCookie.parse(cookieStr, { decodeValues: true });
     var expected = [
-      { name: "foo", value: "R%F3r%EB%80%8DP%FF%3B%2C%23%9A%0CU%8E%A2C8%D7%3C%3C%B0%DF%17%60%F7Y%DB%16%8BQ%D6%1A" },
+      {
+        name: "foo",
+        value:
+          "R%F3r%EB%80%8DP%FF%3B%2C%23%9A%0CU%8E%A2C8%D7%3C%3C%B0%DF%17%60%F7Y%DB%16%8BQ%D6%1A",
+      },
     ];
     assert.deepEqual(actual, expected);
   });
